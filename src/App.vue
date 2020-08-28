f1dd560de4a0d4225933b5c269357feb0b64a729
Aborgen <alexanderborgen@gmail.com>
2020-08-28 16:50:26 -0700

    Create AminoAcidDisplay component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,54 @@
 <template>
 <div class='app'>
-  <codon-handler />
+  <div class='app-container'>
+    <amino-acid-display
+      :amino-acid-chain="aminoAcidChain"
+      :codon-chain="codonChain" />
+    <codon-handler />
+  </div>
 </div>
 </template>
 
 <script>
+import AminoAcidDisplay from 'components/AminoAcidDisplay/AminoAcidDisplay.vue';
 import CodonHandler from 'components/CodonHandler/CodonHandler.vue';
+
 export default {
   name: 'app',
+  data() {
+    return {
+      aminoAcidChain: [],
+      codonChain: []
+    }
+  },
   components: {
+    AminoAcidDisplay,
     CodonHandler
   }
-}
+};
 </script>
 
-<style scoped>
+<style>
 html, body {
   margin: 0;
   padding: 0;
 }
 
 .app {
+  background: #3571a638;
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 30px;
+  margin: 0;
+  padding: 0;
+}
+
+.app-container {
+  width: 70%;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: space-around;
 }
 </style>
---
+++ src/components/AminoAcidDisplay/AminoAcidDisplay.vue
@@ -0,0 +1,44 @@
+<template>
+<div class='amino-acid-container'>
+  <section class='chain-display codon-chain'></section>
+  <section class='chain-display amino-acid-chain'></section>
+  <section class='chain-display extra'></section>
+</div>
+</template>
+
+<script lang='ts'>
+import AminoAcidKey from 'data/amino_acid_key.ts'
+
+export default {
+  name: 'AminoAcidDisplay',
+  props: {
+    aminoAcidChain: {
+      type: Array,
+      required: true
+    },
+    codonChain: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.amino-acid-container {
+  height: 15rem;
+  flex-basis: 40%;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: stretch;
+  justify-content:space-evenly;
+  padding: 2rem;
+}
+
+.chain-display {
+  flex-basis: 23%;
+  background: green;
+
+}
+
+</style>
