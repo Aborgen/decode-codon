f7151a0b2f79ed4e7352136b395356c3fc2db552
Aborgen <alexanderborgen@gmail.com>
2020-08-28 16:46:46 -0700

    Change file type from js to ts

---
+++ src/data/codon_table.ts
@@ -0,0 +1,24 @@
+const data = {
+  "Ala": "Alanine",
+  "Arg": "Arginine",
+  "Asn": "Asparagine",
+  "Asp": "Aspartic Acid",
+  "Cys": "Cysteine",
+  "Glu": "Glutamic Acid",
+  "Gln": "Glutamine",
+  "Gly": "Glycine",
+  "His": "Histidine",
+  "Ile": "Isoleucine",
+  "Leu": "Leucine",
+  "Lys": "Lysine",
+  "Met": "Methionine",
+  "Phe": "Phenylalanine",
+  "Pro": "Proline",
+  "Ser": "Serine",
+  "Thr": "Threonine",
+  "Trp": "Tryptophan",
+  "Tyr": "Tyrosine",
+  "Val": "Valine"
+};
+
+export default data;
