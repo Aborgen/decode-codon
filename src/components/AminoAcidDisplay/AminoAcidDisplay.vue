baea23be042bf2b80877065eba1c6bd89a9088a9
Aborgen <alexanderborgen@gmail.com>
2020-09-23 13:49:36 -0700

    Refactor display areas into new component

--- src/components/AminoAcidDisplay/AminoAcidDisplay.vue
+++ src/components/AminoAcidDisplay/AminoAcidDisplay.vue
@@ -1,35 +1,13 @@
 <template>
 <div class='amino-acid-container'>
-  <section class='chain-display'>
-      <ol class='chain-field' id='codon-field'>
-        <template v-for="(codon, i) in codonChain">
-          <template v-if="i > 0">{{ codonSeparator }}</template>
-          <li :class="{'selected-amino-acid':i === selectedAminoAcid}">{{ codon }}</li>
-        </template>
-      </ol>
-      <button
-        @click="copyCodonsToClipboard"
-        class='copy-button'>copy</button>
-      <select v-model="codonSeparator" name='codon separator'>
-        <option v-for="[separator, description] in Object.entries(possibleSeparators)"
-          :value="separator">{{ description }}</option>
-      </select>
-  </section>
-  <section class='chain-display amino-acid-chain'>
-      <ol class='chain-field' id='amino-acid-field'>
-        <template v-for="(aminoAcid, i) in aminoAcidChain">
-          <template v-if="i > 0">{{ aminoAcidSeparator }}</template>
-          <li :class="{'selected-amino-acid':i === selectedAminoAcid}">{{ aminoAcid }}</li>
-        </template>
-      </ol>
-      <button
-        @click="copyAminoAcidsToClipboard"
-        class='copy-button'>copy</button>
-      <select v-model="aminoAcidSeparator" name='amino acid separator'>
-        <option v-for="[separator, description] in Object.entries(possibleSeparators)"
-          :value="separator">{{ description }}</option>
-      </select>
-  </section>
+  <chain-display
+    :chain="codonChain"
+    :selected-amino-acid="selectedAminoAcid"
+    chain-type="codon" />
+  <chain-display
+    :chain="aminoAcidChain"
+    :selected-amino-acid="selectedAminoAcid"
+    chain-type="amino-acid" />
   <section class='amino-acid-controls'>
     <div class='amino-acid-search'>
       <input
@@ -55,34 +33,7 @@
 </template>
 
 <script lang='ts'>
-// Not finalized... might not work in all instances. clipboard.js?
-function copyToClipboard(s: string) : void {
-  const textarea:HTMLTextAreaElement = document.createElement("textarea");
-  document.body.appendChild(textarea);
-  textarea.value = s;
-  textarea.focus();
-  textarea.select();
-  try {
-    document.execCommand('copy');
-    console.log('Copied to clipboard');
-  }
-  catch (err) {
-    console.log('Unable to copy');
-  }
-  finally {
-    document.body.removeChild(textarea);
-  }
-}
-
-function copyAminoAcidsToClipboard(this: any) {
-  const s:string = this.aminoAcidChain.join(this.aminoAcidSeparator);
-  this.copyToClipboard(s);
-}
-
-function copyCodonsToClipboard(this: any) {
-  const s:string = this.codonChain.join(this.codonSeparator);
-  this.copyToClipboard(s);
-}
+import ChainDisplay from './internal/ChainDisplay/ChainDisplay.vue';
 
 function setSelectedAminoAcid(this: any) : void {
   const input:HTMLInputElement = document.getElementById('search-box') as HTMLInputElement;
@@ -133,16 +84,11 @@ function clearLists(this: any) : void {
 
 export default {
   name: 'AminoAcidDisplay',
+  components: {
+    ChainDisplay
+  },
   data() {
     return {
-      aminoAcidSeparator: '-',
-      codonSeparator: '-',
-      possibleSeparators: {
-        '-' :'dash',
-        ''  :'none',
-        ',' :'comma',
-        '_' :'underscore'
-      },
       searchBoxValue: ''
     };
   },
@@ -165,6 +111,7 @@ export default {
     },
     selectedAminoAcid: {
       type: Number,
+      required: false,
       default: null
     },
     notifyParentToggleEditMode: {
@@ -182,9 +129,6 @@ export default {
   },
   methods: {
     clearLists,
-    copyAminoAcidsToClipboard,
-    copyCodonsToClipboard,
-    copyToClipboard,
     deleteCodon,
     toggleEditMode,
     scrollChains,
@@ -204,25 +148,6 @@ export default {
           selectInput.value = '';
         }
       }
-    },
-    // The purpose of this watcher is to scroll all of the displays to the end when new codons are commited. The only exception
-    // is if any index is selected. In that case, do not scroll.
-    aminoAcidChain: {
-      handler: function(this: any, val: string[], oldVal: string[]) : void {
-        if (this.selectedAminoAcid !== null) {
-          return;
-        }
-
-        this.$nextTick(() => {
-          document.querySelectorAll('.chain-field').forEach((field) => {
-            if (!(field instanceof HTMLElement)) {
-              return;
-            }
-
-            field.scrollLeft = field.scrollWidth;
-          });
-        });
-      }
     }
   }
 };
@@ -240,49 +165,6 @@ export default {
   padding: 2rem;
 }
 
-.chain-display {
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-evenly;
-  align-items: center;
-  flex: 0 0 23%;
-  background: green;
-  font-family: monospace;
-}
-
-.chain-field {
-  flex: 0 1 70%;
-  display: inline-flex;
-  flex-flow: row nowrap;
-  justify-content: flex-start;
-  align-items: center;
-  background: #fff;
-  height: 80%;
-  border: 2px inset black;
-  box-sizing: border-box;
-  font-size: 0.7em;
-  padding: 0;
-  margin: 0;
-  white-space: nowrap;
-  overflow-x: scroll;
-  list-style-type: none;
-}
-
-.chain-field li {
-  display: inline;
-}
-
-.selected-amino-acid {
-  background: orange;
-}
-
-.copy-button {
-  width: 4em;
-  font-size: 0.7em;
-  padding: 0;
-  margin: 0;
-}
-
 .amino-acid-controls {
   flex-basis: 40%;
   display: flex;
@@ -297,5 +179,4 @@ export default {
 #amino-acid-clear {
   font-size: 0.8em;
 }
-
 </style>
---
+++ src/components/AminoAcidDisplay/internal/ChainDisplay/ChainDisplay.vue
@@ -0,0 +1,136 @@
+<template>
+<section :id="`${chainType}-chain`" class='chain-display'>
+    <ol class='chain-field'>
+      <template v-for="(link, i) in chain">
+        <template v-if="i > 0">{{ separator }}</template>
+        <li :class="{'selected-amino-acid':i === selectedAminoAcid}">{{ link }}</li>
+      </template>
+    </ol>
+    <button
+      @click="copyToClipboard"
+      class='copy-button'>copy</button>
+    <select v-model="separator" name='list separator'>
+      <option v-for="[separator, description] in Object.entries(possibleSeparators)"
+        :value="separator">{{ description }}</option>
+    </select>
+</section>
+</template>
+
+<script lang='ts'>
+// Not finalized... might not work in all instances. clipboard.js?
+function copyToClipboard(this:any) : void {
+  const s:string = this.chain.join(this.separator);
+  const textarea:HTMLTextAreaElement = document.createElement("textarea");
+  document.body.appendChild(textarea);
+  textarea.value = s;
+  textarea.focus();
+  textarea.select();
+  try {
+    document.execCommand('copy');
+    console.log('Copied to clipboard');
+  }
+  catch (err) {
+    console.log('Unable to copy');
+  }
+  finally {
+    document.body.removeChild(textarea);
+  }
+}
+
+export default {
+  name: 'ChainDisplay',
+  data() {
+    return {
+      separator: '-',
+      possibleSeparators: {
+        '-' :'dash',
+        ''  :'none',
+        ',' :'comma',
+        '_' :'underscore'
+      }
+    };
+  },
+  props: {
+    chain: {
+      type: Array,
+      required: true
+    },
+    chainType: {
+      type: String,
+      required: true
+    },
+    selectedAminoAcid: {
+      type: Number,
+      required: false,
+      default: null
+    }
+  },
+  methods: {
+    copyToClipboard
+  },
+  watch: {
+    chain: {
+      handler: function(this: any, val: string[], oldVal: string[]) : void {
+        if (this.selectedAminoAcid !== null) {
+          return;
+        }
+
+        this.$nextTick(() => {
+          const field = document.querySelector(`#${this.chainType}-chain .chain-field`);
+          if (!(field instanceof HTMLElement)) {
+            return;
+          }
+
+          field.scrollLeft = field.scrollWidth;
+        });
+      }
+    }
+  }
+};
+
+</script>
+
+<style scoped>
+.chain-display {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-evenly;
+  align-items: center;
+  flex: 0 0 23%;
+  background: green;
+  font-family: monospace;
+}
+
+.chain-field {
+  flex: 0 1 70%;
+  display: inline-flex;
+  flex-flow: row nowrap;
+  justify-content: flex-start;
+  align-items: center;
+  background: #fff;
+  height: 80%;
+  border: 2px inset black;
+  box-sizing: border-box;
+  font-size: 0.7em;
+  padding: 0;
+  margin: 0;
+  white-space: nowrap;
+  overflow-x: scroll;
+  list-style-type: none;
+}
+
+.chain-field li {
+  display: inline;
+}
+
+.selected-amino-acid {
+  background: orange;
+}
+
+.copy-button {
+  width: 4em;
+  font-size: 0.7em;
+  padding: 0;
+  margin: 0;
+}
+</style>
