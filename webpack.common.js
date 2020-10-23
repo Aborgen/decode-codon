39db416e8ee7ed6932c13b65aa7d27f2e477ba76
Aborgen <alexanderborgen@gmail.com>
2020-10-23 15:00:56 -0700

    Separate webpack configuration into common, dev, and production files

---
+++ webpack.common.js
@@ -0,0 +1,52 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  entry: './src/main.ts',
+  output: {
+    filename: 'app.out.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: [/\.vue$/]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, 'src/components'),
+      data: path.resolve(__dirname, 'src/data'),
+      types: path.resolve(__dirname, 'src/types'),
+      tests: path.resolve(__dirname, 'tests')
+    }
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ]
+};
