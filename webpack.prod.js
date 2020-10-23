9f5b8a31533a4292106b3c7d4de87f8e9bf58e17
Aborgen <alexanderborgen@gmail.com>
2020-10-23 15:15:17 -0700

    Update webpack production config to minimize css

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,21 @@
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map'
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin({
+        parallel: true,
+        sourceMap: true,
+        minimizerOptions: {
+          preset: [
+            'default', { discardComments: { removeAll: true } }
+          ]
+        }
+      })
+    ]
+  }
 });
